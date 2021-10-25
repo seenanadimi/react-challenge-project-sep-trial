c3565fdfc2b8974268c91b835e3b30b678f7dbd3
seenanadimi <78619195+seenanadimi@users.noreply.github.com>
2021-10-25 00:03:22 -0700

    feat(route guard): updated filename

--- application/src/router/appRouter.js
+++ application/src/router/appRouter.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Main, Login, OrderFormHook, ViewOrdersHook } from "../components";
-import GaurdedRoute from "./gaurdedRoute/guardedRoute";
+import GuardedRoute from "./guardedRoute";
 
 const AppRouter = (props) => {
   return (
     <Router>
       <Route path="/" exact component={Main} />
       <Route path="/login" exact component={Login} />
-      <GaurdedRoute path="/order" exact component={OrderFormHook} />
-      <GaurdedRoute path="/view-orders" exact component={ViewOrdersHook} />
+      <GuardedRoute path="/order" exact component={OrderFormHook} />
+      <GuardedRoute path="/view-orders" exact component={ViewOrdersHook} />
     </Router>
   );
 };
